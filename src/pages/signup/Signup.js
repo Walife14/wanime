4f0b2fe4fdc07c2de33b99f35248aad6aae2c430
Lucas <58853488+Walife14@users.noreply.github.com>
2021-12-03 14:28:52 +0000

    Created Signup form
    Created the form for the signup - very basic and will make it better in the future, now I need to add the connection to the firestore auth, and create the actual login/signup system.
    
    But first I will add the login form.

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -2,8 +2,30 @@ import styles from './Signup.module.css'
 
 export default function Signup() {
     return (
-        <div>
-            Signup Page
-        </div>
+        <form class={styles.form}>
+            <h2>Crew Application</h2>
+            <label className={styles.label}>
+                <span>Username:</span>
+                <input
+                    type="text"
+                    required
+                />
+            </label>
+            <label className={styles.label}>
+                <span>Email:</span>
+                <input
+                    type="email"
+                    required
+                />
+            </label>
+            <label className={styles.label}>
+                <span>Password:</span>
+                <input
+                    type="password"
+                    required
+                />
+            </label>
+            <button>Signup</button>
+        </form>
     )
 }
