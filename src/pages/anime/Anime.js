2a2a8118ff8d215dfa92c48c3e2f0ed1742eca7d
Lucas <58853488+Walife14@users.noreply.github.com>
2021-12-18 00:47:33 +0000

    Removed liking system
    Realised the liking system would use too many reads on the database and would require too much to implement at the moment.
    
    will save for a later on idea

--- src/pages/anime/Anime.js
+++ src/pages/anime/Anime.js
@@ -1,7 +1,5 @@
 import { useParams } from 'react-router-dom'
-import { useAuthContext } from '../../hooks/useAuthContext'
 import { useDocument } from '../../hooks/useDocument'
-import { useLikeAnime } from '../../hooks/useLikeAnime'
 
 // styles
 import './Anime.css'
@@ -10,30 +8,15 @@ import './Anime.css'
 
 export default function Anime() {
     const { id } = useParams()
-    const { user } = useAuthContext()
     const { document: anime } = useDocument('animes', id)
-    const { document: currentUser } = useDocument('users', user.uid)
-    const { likeAnime, dislikeAnime } = useLikeAnime(user.uid)
-
-    const handleLike = (e) => {
-        if(currentUser.likedAnime.includes(anime.id)) {
-            dislikeAnime(anime.id)
-        }
-        else {
-            likeAnime(anime.id)
-        }
-    }
     
     return (
         <div className="anime-container">
             <div className="anime-profile">
                 <div className="anime-profile-thumb">
-                    {anime && (
-                        <>
-                            <img className="anime-thumbnail" src={anime.thumbnail} alt={`${anime.title} thumbnail`} />
-                            <span className="anime-thumbnail-like" alt="Anime like button" onClick={handleLike}>{anime.likes}</span> 
-                        </>
-                    )}
+                    {anime && 
+                        <img className="anime-thumbnail" src={anime.thumbnail} alt={`${anime.title} thumbnail`} />
+                    }
                 </div>
                 {anime &&
                     <h2>{anime.title}</h2>
